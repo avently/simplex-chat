af981ce329a78f03f0ce50ca5797faa66ae6c2ce
Avently <7953703+avently@users.noreply.github.com>
2023-08-27 15:27:07 +0800

    logs

--- scripts/desktop/build-desktop-mac-ci.sh
+++ scripts/desktop/build-desktop-mac-ci.sh
@@ -10,8 +10,14 @@ echo "desktop.mac.notarization.password=$APPLE_SIMPLEX_NOTARIZATION_PASSWORD" >>
 echo "desktop.mac.notarization.team_id=5NN7GUYB6T" >> apps/multiplatform/local.properties
 echo "$APPLE_SIMPLEX_SIGNING_KEYCHAIN" | base64 --decode - > /tmp/simplex.keychain
 
+date
+echo 1
+security find-certificate -a -c "Developer ID Application: SimpleX Chat Ltd" /tmp/simplex.keychain
+echo 2
 open -a "Keychain Access" /tmp/simplex.keychain
 security unlock-keychain -p "" /tmp/simplex.keychain
+echo 3
+security find-certificate -a -c "Developer ID Application: SimpleX Chat Ltd" /tmp/simplex.keychain
 /usr/bin/codesign -vvvv --timestamp --options runtime --force --prefix "chat.simplex." --sign "Developer ID Application: SimpleX Chat Ltd (5NN7GUYB6T)" --keychain /tmp/simplex.keychain /Users/runner/work/simplex-chat/simplex-chat/apps/multiplatform/local.properties
 
 scripts/desktop/build-lib-mac.sh
